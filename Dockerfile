cbd32809e2408b755f368f3e6423bb216010cbd4
Mun, Younghwan <younghwan.mun@teradata.com>
2020-05-16 14:03:37 -0400

    no message

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /usr/src/app
 
 COPY app/* ./
 
+RUN pip install --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
 
 CMD [ "python", "./hello.py" ]
--- app/wsgi.py
+++ app/wsgi.py
@@ -1,4 +1,10 @@
-from app import app
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return jsonify(hello="world")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", debug=True)
